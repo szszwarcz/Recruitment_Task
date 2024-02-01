45b52bcabc58c6a33527d2cc45ea2e48fbf88a48
Szymon Szwarczynski <szymon.szwarczynski@gmail.com>
2024-02-01 11:52:57 +0100

    Added functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LaunchPadComponent } from './launch-pad/launch-pad.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home-component', pathMatch: 'full' },
+  {path: 'lauchpad-component', component:LaunchPadComponent},
+  {path: 'home-component', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {PageEvent, MatPaginatorModule} from '@angular/material/paginator';
+import {Component} from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list'; 
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { LaunchPadComponent } from './launch-pad/launch-pad.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LaunchPadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    JsonPipe,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
   ],
   providers: [],
   bootstrap: [AppComponent]
